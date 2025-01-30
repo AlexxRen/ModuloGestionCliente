b9c6f6875fea6dbb8e32d908f6ce6b2859681f02
alex rengifo <alexren567@outlook.com>
2025-01-30 17:48:33 -0500

    CRUD de Cliente configuracion para no tener credemciales en codigo solo en el appsettings

--- ModuloGestionCliente/Models/DB/ProyectContext.cs
+++ ModuloGestionCliente/Models/DB/ProyectContext.cs
@@ -6,14 +6,18 @@ namespace ModuloGestionCliente.Models.DB;
 
 public partial class ProyectContext : DbContext
 {
-    public ProyectContext()
+    private readonly IConfiguration _configuration;
+    public ProyectContext(IConfiguration configuration)
     {
+        _configuration = configuration;
     }
 
-    public ProyectContext(DbContextOptions<ProyectContext> options)
+    /*public ProyectContext(DbContextOptions<ProyectContext> options)
         : base(options)
     {
-    }
+
+    }*/
+
 
     public virtual DbSet<Auditoria> Auditorias { get; set; }
 
@@ -28,8 +32,12 @@ public partial class ProyectContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=CASAALEX; Database=Proyect; Trusted_Connection=True;");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
